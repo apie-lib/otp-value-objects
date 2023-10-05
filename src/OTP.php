6c8b18fb57e77cae67ebfcb9b91c4fa75b3094d8
pjordaan <pjordaan@users.noreply.github.com>
2023-10-05 08:07:30 +0000

    Update file(s) "packages/otp-value-objects/." from "apie-lib/apie-lib-monorepo"

--- src/OTP.php
+++ src/OTP.php
@@ -7,13 +7,13 @@ use Apie\Core\ValueObjects\IsStringWithRegexValueObject;
 use Apie\OtpValueObjects\Concerns\NoIndexing;
 
 #[ProvideIndex('noIndexing')]
-final class OTP implements StringValueObjectInterface
+class OTP implements StringValueObjectInterface
 {
     use IsStringWithRegexValueObject;
     use NoIndexing;
 
     public static function getRegularExpression(): string
     {
-        return '/^[\d]{6}$/';
+        return '/^[\d]{6,8}$/';
     }
 }
--- src/VerifyOTP.php
+++ src/VerifyOTP.php
@@ -4,8 +4,12 @@ namespace Apie\OtpValueObjects;
 use Apie\Core\Attributes\ProvideIndex;
 use ReflectionProperty;
 
+/**
+ * Use and extend this class if you want to enable 2FA action by telling the property of the OTP secret
+ * and the label required to generate the QR Code.
+ */
 #[ProvideIndex('noIndexing')]
-abstract class VerifyOtp extends OTP
+abstract class VerifyOTP extends OTP
 {
     /**
      * @return ReflectionProperty
