e8242b3d6b807f92798ae80b096db36c34852e51
pjordaan <pjordaan@users.noreply.github.com>
2024-06-10 20:52:19 +0000

    Update file(s) "packages/otp-value-objects/." from "apie-lib/apie-lib-monorepo"

--- tests/HOTPSecretTest.php
+++ tests/HOTPSecretTest.php
@@ -4,6 +4,7 @@ namespace Apie\Tests\OtpValueObjects;
 use Apie\Fixtures\TestHelpers\TestWithFaker;
 use Apie\OtpValueObjects\HOTPSecret;
 use Apie\Serializer\Exceptions\ValidationException;
+use OTPHP\HOTP;
 use PHPUnit\Framework\TestCase;
 
 class HOTPSecretTest extends TestCase
@@ -89,6 +90,37 @@ class HOTPSecretTest extends TestCase
                 $otp = $instance->createOTP();
                 $this->assertTrue($instance->verify($otp));
             }
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_provide_a_url_from_qrserver()
+    {
+        $secret = str_repeat('A', 103);
+        $tmp = HOTP::create($secret, 0);
+        $testItem = new HOTPSecret($tmp);
+        $this->assertEquals(
+            'https://api.qrserver.com/v1/create-qr-code/?data=otpauth%3A%2F%2Fhotp%2FTestcase%3Fcounter%3D0%26secret%3D' . $secret . '&size=300x300&ecc=M',
+            $testItem->getQrCodeUri('Testcase')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_provide_a_base64_image()
+    {
+        $secret = str_repeat('A', 103);
+        $tmp = HOTP::create($secret, 0);
+        $testItem = new HOTPSecret($tmp);
+        $file = __DIR__ . '/../fixtures/hotp.txt';
+        $actual = $testItem->getUrl('Testcase');
+        // file_put_contents($file, $actual);
+        $this->assertEquals(
+            file_get_contents($file),
+            $actual
         );
     }
 }
--- tests/TOTPSecretTest.php
+++ tests/TOTPSecretTest.php
@@ -31,4 +31,33 @@ class TOTPSecretTest extends TestCase
             }
         );
     }
+
+    /**
+     * @test
+     */
+    public function it_can_provide_a_url_from_qrserver()
+    {
+        $secret = str_repeat('A', 103);
+        $testItem = new TOTPSecret($secret);
+        $this->assertEquals(
+            'https://api.qrserver.com/v1/create-qr-code/?data=otpauth%3A%2F%2Ftotp%2FTestcase%3Fsecret%3D' . $secret . '&size=300x300&ecc=M',
+            $testItem->getQrCodeUri('Testcase')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_provide_a_base64_image()
+    {
+        $secret = str_repeat('A', 103);
+        $testItem = new TOTPSecret($secret);
+        $file = __DIR__ . '/../fixtures/totp.txt';
+        $actual = $testItem->getUrl('Testcase');
+        // file_put_contents($file, $actual);
+        $this->assertEquals(
+            file_get_contents($file),
+            $actual
+        );
+    }
 }
